03fdeb75f5aa550b2fa7a465ae1aee5a693098d1
VadimShulga <vadimshulga@coherentsolutions.com>
2021-07-14 23:24:25 +0300

    fix comments

--- src/test/java/yandex/pages/task60_pageObject/MailRuAccountPage.java
+++ src/test/java/yandex/pages/task60_pageObject/MailRuAccountPage.java
@@ -2,21 +2,22 @@ package yandex.pages.task60_pageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MailRuAccountPage {
     private WebDriver driver;
-    private static final By ACCOUNT_NAME = By.cssSelector("[data-testid='whiteline-account']");
+    private static final By ACCOUNT_NAME = By.xpath("//div[@data-testid='whiteline-account']//span[contains(@class, 'user-name')]");
 
     public MailRuAccountPage(WebDriver driver) {
         this.driver = driver;
-        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(ACCOUNT_NAME));
     }
 
     public AccountMenuModal openMenu() {
         driver.findElement(ACCOUNT_NAME).click();
         return new AccountMenuModal(driver);
+    }
+
+    public String getAccountName() {
+        return driver.findElement(ACCOUNT_NAME).getText();
     }
 
 }
--- src/test/java/yandex/pages/task60_pageObject/MailRuMainPage.java
+++ src/test/java/yandex/pages/task60_pageObject/MailRuMainPage.java
@@ -1,10 +1,7 @@
 package yandex.pages.task60_pageObject;
 
-import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MailRuMainPage {
     private WebDriver driver;
@@ -13,14 +10,12 @@ public class MailRuMainPage {
     private static final By ENTER_PASSWORD_BUTTON = By.cssSelector("[data-testid='enter-password']");
     private static final By ENTER_BUTTON = By.cssSelector("[data-testid='login-to-mail']");
     public static final By LOGO = By.cssSelector(("[data-testid='logo-item']"));
-    private static final String URL = "https://mail.ru/";
 
     public MailRuMainPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public MailRuAccountPage login(String username, String password) {
-        driver.get(URL);
         driver.findElement(USERNAME_INPUT).sendKeys(username);
         driver.findElement(ENTER_PASSWORD_BUTTON).click();
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
@@ -28,9 +23,9 @@ public class MailRuMainPage {
         return new MailRuAccountPage(driver);
     }
 
-    public MailRuMainPage logoIsAppeared() {
-        Assertions.assertTrue(driver.findElement(LOGO).isDisplayed());
-        return this;
+    public boolean logoIsAppeared() {
+        boolean isAppeared = driver.findElement(LOGO).isDisplayed();
+        return isAppeared;
     }
 }
 
