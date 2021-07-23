8592c24f7b356e4d5ae075fc9d433567bbd8ef54
VadimShulga <vadimshulga@coherentsolutions.com>
2021-07-23 14:31:58 +0300

    add task100

---
+++ src/test/java/utils/MyTestWatcher.java
@@ -0,0 +1,46 @@
+package utils;
+
+import io.qameta.allure.Attachment;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import yandex.tests.wevdriver.WebDriverSingleton;
+
+import java.util.Optional;
+
+
+@Log4j2
+public class MyTestWatcher implements TestWatcher {
+
+    @Override
+    public void testAborted(ExtensionContext extensionContext, Throwable throwable) {
+        log.info("Test Aborted for test {}: ", extensionContext.getDisplayName());
+    }
+
+    @Override
+    public void testDisabled(ExtensionContext extensionContext, Optional<String> optional) {
+        log.info("Test Disabled for test {}: with reason :- {}",
+                extensionContext.getDisplayName(),
+                optional.orElse("No reason"));
+    }
+
+    @Override
+    public void testFailed(ExtensionContext extensionContext, Throwable e) {
+        log.info("Test Disabled for test {}: with reason :- {}",
+                extensionContext.getDisplayName());
+        takeScreenshot();
+    }
+
+    @Override
+    public void testSuccessful(ExtensionContext extensionContext) {
+        log.info("Test Successful for test {}: ", extensionContext.getDisplayName());
+    }
+
+    @Attachment(value = "Page Screenshot", type = "image/png")
+    public static byte[] takeScreenshot() {
+        return ((TakesScreenshot)WebDriverSingleton.getInstance()).getScreenshotAs(OutputType.BYTES);
+    }
+}
--- src/test/java/yandex/tests/task60_pageObject/MailRuPageTest.java
+++ src/test/java/yandex/tests/task60_pageObject/MailRuPageTest.java
@@ -1,7 +1,10 @@
 package yandex.tests.task60_pageObject;
 
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+import utils.MyTestWatcher;
 import utils.ScreenShot;
 import yandex.pages.task60_pageObject.AccountMenuModal;
 import yandex.pages.task60_pageObject.MailRuAccountPage;
@@ -9,8 +12,11 @@ import yandex.pages.task60_pageObject.MailRuMainPage;
 import yandex.tests.wevdriver.WebDriverSingleton;
 
 import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MyTestWatcher.class)
 public class MailRuPageTest {
 
     private WebDriver driver;
@@ -21,13 +27,13 @@ public class MailRuPageTest {
     private final static String USERNAME = "seleniumtests";
     private final static String PASSWORD = "OYAY43rtpty$";
     private static final String URL = "https://mail.ru/";
-    String folderForScreenshots = Paths.get("src", "test", "resources", "screen.jpg").toString();
+    String timestamp = new SimpleDateFormat("yyyy_MM_dd__hh_mm_ss").format(new Date());
+    String folderForScreenshots = Paths.get("target", "screen" + timestamp + ".jpg").toString();
+    private MyTestWatcher myTestWatcher = new MyTestWatcher();
 
     @BeforeEach
     public void setUp() {
         driver = WebDriverSingleton.getInstance();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
         mailRuMainPage = new MailRuMainPage(driver);
         accountMenuModal = new AccountMenuModal(driver);
         mailRuAccountPage = new MailRuAccountPage(driver);
@@ -36,17 +42,19 @@ public class MailRuPageTest {
     }
 
     @Test
+    @Description("1. Login test with correct credentials")
     public void correctLoginTest() {
         mailRuAccountPage = mailRuMainPage.login(USERNAME, PASSWORD);
-        screenShot.takeSnapShot(driver, folderForScreenshots);
+//        screenShot.takeSnapShot(driver, folderForScreenshots);
         Assertions.assertEquals(USERNAME + "@mail.ru", mailRuAccountPage.getAccountName());
     }
 
     @Test
+    @Description("2. Logout test")
     public void logOutTest() {
         mailRuMainPage
                 .login(USERNAME, PASSWORD);
-        screenShot.takeSnapShot(driver, folderForScreenshots);
+//        screenShot.takeSnapShot(driver, folderForScreenshots);
         mailRuAccountPage
                 .openMenu();
         accountMenuModal
@@ -57,7 +65,7 @@ public class MailRuPageTest {
     @AfterEach
     public void closeBrowser() {
         if (driver != null) {
-            driver.quit();
+            WebDriverSingleton.closeBrowser();
         }
     }
 }
